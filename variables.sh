1b213110277452d1ccc4fc0bed6e4944cf88e72b
naveenreddycloud <devopscloud431@gmail.com>
2024-08-23 15:21:29 +0530

    learning variables scripit

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# variables shell scripiting
+
+person1=ramesh
+person2=mahesh
+
+echo "$person1:: hi $person2, how are you ? where are you?"
+echo "$person2:: hi $person1, i am fine . now i am in managalgiri"
