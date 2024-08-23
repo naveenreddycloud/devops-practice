96fbae98826435e2edabb6645f3547d7ebfe95ae
naveenreddycloud <devopscloud431@gmail.com>
2024-08-23 15:38:04 +0530

    read variables

---
+++ read_variable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# read variables shell scripit
+
+echo "please enter your username::"
+
+read USERNAME
+
+echo "username entered is :: $USERNAME"
+
+ECHO "PLEASE ENTER YOUR PASSWORD::"
+
+read PASSWORD
+
+echo "password is ::$PASWWORD"
+ 
