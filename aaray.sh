73b05c11eb8fe5fd36635f2129890c7a8e3d4526
naveenreddycloud <devopscloud431@gmail.com>
2024-08-23 16:22:36 +0530

    learning aary

---
+++ aaray.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#learning array concept
+
+FRUITS=("APPLE", "MANGO" , "BANNANNA")
+
+echo "first fruit is :${FRUITS[0]}"
+echo "seconf fruits is:${FRUITS[1]}"
+echo "third fruit is :${FRUITS[2]}"
+
+echo  "fruit is ${FRUITS[@]}"
