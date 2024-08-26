992aa7e45a2465d22e3c87854ce5555cfcc0751a
naveenreddycloud <devopscloud431@gmail.com>
2024-08-26 16:18:30 +0530

    fun

---
+++ function.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#function scripit sample
+
+hello(){
+
+    echo "hi navven $1"
+    echo "scripit name $0"
+    echo "number of argus $#"
+    echo "All are argus $@"
+
+       }
+       
