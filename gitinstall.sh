f89592c84fe68ef063cadc44a1f6ebd099250da9
naveenreddycloud <devopscloud431@gmail.com>
2024-08-26 15:56:53 +0530

    gitin

---
+++ gitinstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]; then
+
+echo "must be root user to acces"
+exit1
+
+fi
+
+yum install git -y 
+
+if [ USERID -ne 0 ]; then 
+
+echo "installing git failure"
+exit1
+
+else "installing git"
+
+fi
