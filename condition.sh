2ae5b5edd681069eb90dfa90a2b08f58494d3b51
naveenreddycloud <devopscloud431@gmail.com>
2024-08-26 15:37:00 +0530

    upcon1

--- condition.sh
+++ condition.sh
@@ -5,5 +5,17 @@ ls -ltr
 if [ $? -ne 0 ]; then 
   
   echo "above command failure"
+  exit 1
 
   fi
+
+  chmod go -rwx condition.sh
+
+  if [ $? -ne 0 ]; then 
+  
+  echo "above command failure"
+  exit 1
+
+  fi
+
+
